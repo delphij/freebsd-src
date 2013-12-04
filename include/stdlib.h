40999f77cf4580c707d8631c336033a1896447cf
Xin Li <delphij@FreeBSD.org>
2013-12-03 17:00:06 -0800

    Don't call arc4random_stir() manually.

--- include/stdlib.h
+++ include/stdlib.h
@@ -259,9 +259,7 @@ void	*alloca(size_t);
 void	 abort2(const char *, int, void **) __dead2;
 __uint32_t
 	 arc4random(void);
-void	 arc4random_addrandom(unsigned char *, int);
 void	 arc4random_buf(void *, size_t);
-void	 arc4random_stir(void);
 __uint32_t 
 	 arc4random_uniform(__uint32_t);
 char	*getbsize(int *, long *);
